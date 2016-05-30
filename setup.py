c1341e85aa83980a29d747201cf7175ae3eb6a79
Sébastien <seb.bertram@outlook.com>
2016-05-30 23:58:14 +0200

    change setyp.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 # coding: utf-8
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='payByPhone_calendar',
     version='0.1',
     url='https://github.com/Ajira-FR/payByPhone_calendar',
-    license='',
+    license='GPL',
     author='Sebastien',
     author_email='',
     description='An HTML calendar with a Python back-end to manage PayByPhone parking ticket',
-    packages=['bottle', 'bottle_sqlite', 'bs4', 'sqlite3', 'requests']
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=['bottle', 'bottle_sqlite', 'bs4', 'sqlite3', 'requests']
 )
